4aa0a7ba62d622d24a7b409509476c6ed7f7cddf
Marisol Colon <mcolon@foundationmedicine.com>
2020-09-23 10:19:10 -0500

    adding code for print the value based on the key

--- src/Class2.java
+++ src/Class2.java
@@ -16,17 +16,18 @@ public class Class2 {
         System.out.println("--------->");
 
         // trough a HashMap and print the value based on the Key
-//        HashMap<Integer, String> testHashMap = new HashMap<>();
-//        testHashMap.put(18, "Alexa");
-//        testHashMap.put(56, "Marisol");
-//        testHashMap.put(34, "Kassandra");
-//
-//        String test1 = testHashMap.get(18);
-//        System.out.println("The Value mapped to Key 18 is: "+ test1);
-//
-//        String test2 = testHashMap.get(4);
-//        System.out.println("The Value mapped to Key 4 is: "+ test2);
-//        System.out.println("--------->");
+        HashMap<Integer, String> testHashMap = new HashMap<>();
+        testHashMap.put(18, "Alexa");
+        testHashMap.put(56, "Marisol");
+        testHashMap.put(34, "Kassandra");
+
+        String test1 = testHashMap.get(18);
+        System.out.println("The Value mapped to Key 18 is: "+ test1);
+
+        String test2 = testHashMap.get(56);
+        System.out.println("The Value mapped to Key 56 is: "+ test2);
+
+        testHashMap.forEach((key, value) -> System.out.println(key + " : " + value));
 
     }
 
